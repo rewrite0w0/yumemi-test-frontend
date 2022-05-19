5ac632869fe859c3e53cabecce5aec6dd5683b5c
Oh TaeJun <rewrite0w0@gmail.com>
2022-05-19 19:12:02 +0900

    mod: asset prefix

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     loader: 'akamai',
     path: '',
   },
-  assetPrefix: isProd ? '/yumemi-test-frontend/' : '',
+  // assetPrefix: isProd ? '/yumemi-test-frontend/' : '',
   // assetPrefix: !debug ? "/yumemi-frontend-test/" : '',
 }
 
